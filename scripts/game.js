7f9ed136fdcd5fb508b15c761a088d214d7a0a07
Felix R <nikojin@gmx.net>
2025-10-10 11:23:08 +0200

    add gitignore

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+let canvas;
+let character = new Image();
+
+function init() {
+    canvas = document.getElementById("canvas");
+    character.src = 'assets/img/'
+    // const canvas = document.querySelector("canvas");
+    // const ctx = canvas.getContext("2d");
+}
