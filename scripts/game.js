a71b5a6c9d7a7436590d51c4d8180c0b55247270
Felix R <nikojin@gmx.net>
2025-10-10 13:31:54 +0200

    upd models

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,15 @@
 let canvas;
 let ctx;
-let character = new MovableObject();
+let character = new Character();
 
 function init() {
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
-    character.src = '../assets/img/2BlueWizardIdle/Chara - BlueIdle00000.png';
-    character.onload = function() {
-        // ctx.imageSmoothingEnabled = false;
-        ctx.drawImage(character, 20, 300, 100, 100);
-    }
+    // character.src = '../assets/img/2BlueWizardIdle/Chara - BlueIdle00000.png';
+    // character.onload = function() {
+    // ctx.imageSmoothingEnabled = false;
+    //     ctx.drawImage(character, 20, 300, 100, 100);
+    // }
 
     console.log("MyCharacter is", character);
     
