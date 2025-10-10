4371434d99324a1e9962823f5b39340246b446ab
Felix R <nikojin@gmx.net>
2025-10-10 12:55:48 +0200

    add Idle Img

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,14 @@
 let canvas;
+let ctx;
 let character = new Image();
 
 function init() {
     canvas = document.getElementById("canvas");
-    character.src = 'assets/img/'
-    // const canvas = document.querySelector("canvas");
-    // const ctx = canvas.getContext("2d");
+    ctx = canvas.getContext("2d");
+    character.src = '../assets/img/2BlueWizardIdle/Chara - BlueIdle00000.png';
+    character.onload = function() {
+        // ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(character, 20, 60, 100, 100);
+    }
+   
 }
