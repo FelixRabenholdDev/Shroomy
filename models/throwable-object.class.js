4699d52e55ae183cb98b76eb6394dcce3dc61038
Felix R <nikojin@gmx.net>
2025-10-27 13:38:01 +0100

    add throw animation

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -9,20 +9,39 @@ class ThrowableObject extends MovableObject {
     ]
 
     constructor(x, y) {
-        super().loadImage('../assets/img/Bullet/Bullet1.png');
+        super().loadImage(this.Ball_Images[0]);
         this.x = x;
         this.y = y;
         this.width = 20;
         this.height = 20;
-        this.throw();
-        // this.loadImages(this.Ball_Images);
+        this.loadImages(this.Ball_Images);
+        this.throw();        
     }
 
     throw() {        
-        this.speedY = 10;
+        this.speedY = 7;
         this.applyGravity();
         setInterval(() => {
-            this.x += 7;
+            this.x += 10;
         }, 25);
-    }    
+        this.animateThrow();
+    }
+
+    animateThrow() {
+        this.animationInterval = setInterval(() => {
+            const gradient = this.speedY;
+
+            if (gradient > 6) {
+                this.playAnimation([this.Ball_Images[0]]); 
+            } else if (gradient > 2) {
+                this.playAnimation([this.Ball_Images[1]]);
+            } else if (gradient > -2) {
+                this.playAnimation([this.Ball_Images[2]]); 
+            } else if (gradient > -6) {
+                this.playAnimation([this.Ball_Images[3]]);
+            } else {
+                this.playAnimation([this.Ball_Images[4]]);
+            }
+        }, 50);
+    }
 }
